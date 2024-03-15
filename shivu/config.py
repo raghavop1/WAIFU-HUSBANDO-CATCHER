f3001c3f6b89a4a9c9d7c3b804cc68a6d9b93d44
raghavop1 <150908224+raghavop1@users.noreply.github.com>
2024-03-15 13:06:08 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,7 +3,7 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6325295720"
-    sudo_users = 
+    sudo_users = "6768723634"
     GROUP_ID = "-1002100894623"
     TOKEN = "7055204971:AAHbREm4BeNyKXxXWidGWA4jXqn9KHKZVIo"
     mongo_url = "mongodb+srv://raghavvvv09:DpaQVcz8AJW8grFX@cluster0.edbvlbl.mongodb.net/?retryWrites=true&w=majority"
